f0c819db7b9856719a92d9951025342fa49fca24
IvanMikey <ivan.mikey1@gmail.com>
2025-05-01 11:09:38 +0300

    fix: simplify loading screen and remove unnecessary redirect scripts

--- src/components/LoadingScreen/LoadingScreen.js
+++ src/components/LoadingScreen/LoadingScreen.js
@@ -1,24 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './style.css'
+import React from 'react';
+import './style.css';
 
 const LoadingScreen = () => {
-  const navigate = useNavigate();
-  const [showLoader, setShowLoader] = useState(true);
-
-  useEffect(() => {
-    const loadingTimer = setTimeout(() => {
-      setShowLoader(false);
-      navigate('/home');
-    }, 7000);
-
-    return () => {
-      clearTimeout(loadingTimer);
-    };
-  }, [navigate]);
-
   return (
-    <div className={`fixed top-0 left-0 w-full h-full bg-gray-800 flex items-center justify-center transition-opacity ${showLoader ? 'opacity-100' : 'opacity-0'}`}>
+    <div className="fixed top-0 left-0 w-full h-full bg-gray-800 flex items-center justify-center">
       <div className="loader"></div>
     </div>
   );
