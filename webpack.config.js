5ce5a80f59d20ff8883785b01cc67de67746f7de
IvanMikey <ivan.mikey1@gmail.com>
2025-05-01 13:09:28 +0300

    fix: optimize build configuration and routing

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,39 @@
 // webpack.config.js
+const path = require('path');
+
 module.exports = {
-    // ... other configurations
-    resolve: {
-      alias: {
-        'swiper': 'swiper/bundle', // or the correct path to the swiper bundle
-      },
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'static/js/[name].[contenthash:8].js',
+        publicPath: '/portfolio/'
     },
-  };
+    resolve: {
+        alias: {
+            'swiper': 'swiper/bundle', // or the correct path to the swiper bundle
+        },
+        extensions: ['.js', '.jsx', '.json']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader']
+            }
+        ]
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+            name: false
+        }
+    }
+};
